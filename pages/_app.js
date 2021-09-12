37e2fb1e1563c5da6b75544eb94f050642f41a53
Facundo Brahim <facundo.e.brahim@gmail.com>
2021-09-12 18:02:58 -0300

    GlobalStyles.js Fixed

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,16 @@
-import { useState } from "react";
-import { lightTheme, darkTheme, GlobalStyles } from "../constants/themeConfig";
-import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "../constants/globalStyles";
 import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  const [theme, setTheme] = useState("light");
-
-  const toggleTheme = () => {
-    theme == "light" ? setTheme("dark") : setTheme("light");
-  };
-
   return (
     <>
-      <ThemeProvider theme={theme == "light" ? lightTheme : darkTheme}>
-        <Head>
-          <title>Tesis - Facundo Brahim</title>
-          <meta name="description" content="Tesis - Facundo Brahim" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <button onClick={toggleTheme}>Switch Theme</button>
-        <Component {...pageProps} />
-        {/* Global CSS */}
-      </ThemeProvider>
+      <Head>
+        <title>Tesis - Facundo Brahim</title>
+        <meta name="description" content="Tesis - Facundo Brahim" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+      <GlobalStyles />
     </>
   );
 }
